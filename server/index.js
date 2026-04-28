4f8042722a6ca5f215d67104368837e3eca51714
rafshanrialmollick <rafshanrialmollick@gmail.com>
2026-04-28 17:50:54 +0600

    initialize the project

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const dayjs = require('dayjs');
+
+console.log(dayjs().format('YYYY-MM-DD'));        // 2024-01-15
+console.log(dayjs().format('DD/MM/YYYY'));        // 15/01/2024
+console.log(dayjs().format('hh:mm:ss'));          // 10:30:25
+
+// নির্দিষ্ট date বানাও
+console.log(dayjs('2024-01-15').format('DD MMM YYYY')); // 15 Jan 2024
+
+// আজকের দিন থেকে যোগ বিয়োগ
+console.log(dayjs().add(7, 'day').format('YYYY-MM-DD'));      // ৭ দিন পরে
+console.log(dayjs().subtract(1, 'month').format('YYYY-MM-DD')); // ১ মাস আগে
